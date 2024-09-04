826880684cd435b80b64df1d2146001bd4699916
Tahsinul Islam <125194762+Tahsin123387@users.noreply.github.com>
2024-09-04 13:37:53 +0600

    Update Test_Cases.cpp

--- Test_Cases.cpp
+++ Test_Cases.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 
 // Function to multiply two matrices
-vector<vector<int>> matrixMultiply(const vector<vector<int>>& A, const vector<vector<int>>& B) {
-    int rowsA = A.size();      
-    int colsA = A[0].size();  
-    int rowsB = B.size();     
-    int colsB = B[0].size();  
+vector<vector<double>> matrixMultiply(const vector<vector<double>>& A, const vector<vector<double>>& B) {
+    int rowsA = A.size();        
+    int colsA = A[0].size();    
+    int rowsB = B.size();        
+    int colsB = B[0].size();   
 
     // Check if matrices can be multiplied
     if (colsA != rowsB) {
         cerr << "The two matrices can't be multiplied because the number of columns in the first matrix doesn't match the number of rows in the second matrix.\n" << endl;
-        return {};  // Return an empty vector if matrices can't be multiplied
+        return {};  
     }
 
     // Initialize the result matrix with zeros
-    vector<vector<int>> result(rowsA, vector<int>(colsB, 0));
+    vector<vector<double>> result(rowsA, vector<double>(colsB, 0.0));
 
     // Perform matrix multiplication
     for (int i = 0; i < rowsA; ++i) {
@@ -32,10 +32,10 @@ vector<vector<int>> matrixMultiply(const vector<vector<int>>& A, const vector<ve
 }
 
 // Function to print a matrix
-void printMatrix(const vector<vector<int>>& matrix, const string& name) {
+void printMatrix(const vector<vector<double>>& matrix, const string& name) {
     cout << "Matrix " << name << ":\n";
     for (const auto & row : matrix) {
-        for (int elem : row) {
+        for (double elem : row) {
             cout << elem << " ";
         }
         cout << endl;
@@ -44,46 +44,47 @@ void printMatrix(const vector<vector<int>>& matrix, const string& name) {
 }
 
 int main() {
-    vector<vector<int>> A = {
+    vector<vector<double>> A = {
         {1, 2, 3},
         {4, 5, 6}
     };
 
-    vector<vector<int>> B = {
+    vector<vector<double>> B = {
         {7, 8},
         {9, 10},
         {11, 12}
     };
 
-    cout<<"Test Case 1\n";
+    cout << "Test Case 1\n";
     printMatrix(A, "A");
     printMatrix(B, "B");
 
-    vector<vector<int>> resultAB = matrixMultiply(A, B);
+    vector<vector<double>> resultAB = matrixMultiply(A, B);
 
     if (!resultAB.empty()) {
         printMatrix(resultAB, "C (A * B)");
     }
 
-    vector<vector<int>> X = {
+    vector<vector<double>> X = {
         {1, 2, 3},
         {4, 5, 6},
         {7, 8, 9}
     };
 
-    vector<vector<int>> Y = {
+    vector<vector<double>> Y = {
         {10, 11, 12},
         {13, 14, 15}
-
     };
-    cout<<"Test Case 2\n";
+
+    cout << "Test Case 2\n";
     printMatrix(X, "X");
     printMatrix(Y, "Y");
 
-    vector<vector<int>> resultXY = matrixMultiply(X, Y);
+    vector<vector<double>> resultXY = matrixMultiply(X, Y);
 
     if (!resultXY.empty()) {
         printMatrix(resultXY, "Z (X * Y)");
     }
+
     return 0;
 }
